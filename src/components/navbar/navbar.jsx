450cd110b0b17bcd9cc9a1aff825a5332a6d59cf
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-10-29 21:29:53 +0430

    modify navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,14 @@
 import "./navbar.scss"
 const Navbar = () => {
   return (
-    <div className="navbar">navbar</div>
+      <div className="navbar">
+          <div className="wrapper">
+              <span>Ali Dev</span>
+              <div className="social">
+                  <a href="#"><img src="" alt="" /></a>
+              </div>
+          </div>
+    </div>
   )
 }
 
