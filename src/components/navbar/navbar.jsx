9c4f19a1cf0dd39dac58491727bd6a2f37e68015
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-17 10:51:07 +0430

    Add social to navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -12,7 +12,10 @@ const Navbar = () => {
                 Ali Dev
               </motion.span>
               <div className="social">
-                  <a href="#"><img src="" alt="" /></a>
+                  <a href="#"><img src="/facebook.png" alt="" /></a>
+                  <a href="#"><img src="/instagram.png" alt="" /></a>
+                  <a href="#"><img src="/youtube.png" alt="" /></a>
+                  <a href="#"><img src="/dribble.png" alt="" /></a>
               </div>
           </div>
     </div>
