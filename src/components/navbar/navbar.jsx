c54487a090b76f30371795497b0f67eaebbd3804
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-17 10:46:03 +0430

    Modified navbar jsx component

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,16 @@
 import "./navbar.scss"
+import {motion} from 'framer-motion'
 
 const Navbar = () => {
   return (
       <div className="navbar">
           <div className="wrapper">
-              <span>Ali Dev</span>
+              <motion.span 
+              initial={{opacity: 0, scale: 0.5}} 
+              animate={{opacity: 1, scale: 1}}
+              transition={{duration: 0.5}} >
+                Ali Dev
+              </motion.span>
               <div className="social">
                   <a href="#"><img src="" alt="" /></a>
               </div>
