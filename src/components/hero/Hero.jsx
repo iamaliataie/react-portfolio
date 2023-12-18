c11c00619509a1c85ba6ed4af636fd85ab6bf5b0
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-12-18 20:36:24 +0430

    Update Hero component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,21 @@
 import './hero.scss'
 const Hero = () => {
+
+  const textVariants = {
+    initial: {
+      x: -50,
+      opacity: 0
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.1
+      }
+    }
+  }
+
   return (
     <div className='hero'>
       <div className="wrapper">
