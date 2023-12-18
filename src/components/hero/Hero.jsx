6b78183bb695ea563c20486b3901b14b8200e373
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-18 14:17:44 +0430

    Add hero texts

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,6 +2,17 @@ import './hero.scss'
 const Hero = () => {
   return (
     <div className='hero'>
+      <div className="wrapper">
+        <div className="text-container">
+          <h2>HARVEY TYLER</h2>
+          <h1>Web Developer and UI Designer</h1>
+          <div className="buttons">
+            <button>See the latest works</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="./scroll.png" alt="" />
+        </div>
+      </div>
       <div className="image-container">
         <img src="./hero.png" alt="" srcset="" />
       </div>
