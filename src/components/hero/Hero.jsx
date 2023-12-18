491577fb04564c1e746ff1146055b266370d52f1
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-12-18 20:24:50 +0430

    merge

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,25 @@
 import './hero.scss'
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className="wrapper">
+        <div className="text-container">
+          <h2>HARVEY TYLER</h2>
+          <h1>Web Developer and UI Designer</h1>
+          <div className="buttons">
+            <button>See the latest works</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="./scroll.png" alt="" />
+        </div>
+      </div>
+      <div className="sliding-text-container">
+        Writer Content Creator Influencer
+      </div>
+      <div className="image-container">
+        <img src="./hero.png" alt="" srcset="" />
+      </div>
+    </div>
   )
 }
 
