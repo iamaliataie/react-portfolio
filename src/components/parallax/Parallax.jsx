c346bbc8089c60a44a2db0749d6fb4d0f530e1a2
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-21 10:45:23 +0430

    Update Pallax jsx

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -4,6 +4,9 @@ const Parallax = ({type}) => {
   return (
     <div className='parallax'>
         <h1>{type == 'services' ? 'What We Do?' : 'What We Did?'}</h1>
+        <div className="mountains"></div>
+        <div className="planets"></div>
+        <div className="stars"></div>
     </div>
   )
 }
