87060c7c978080becdf134ab77b9df9232d6ade7
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-21 10:48:41 +0430

    Update Pallax jsx

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -2,7 +2,7 @@ import './parallax.scss';
 
 const Parallax = ({type}) => {
   return (
-    <div className='parallax'>
+    <div className='parallax' style={{background: type==='services' ? 'linear-gradient(180deg, #111132, #0c0c1d)' : 'linear-gradient(180deg, #111132, #505064)'}}>
         <h1>{type == 'services' ? 'What We Do?' : 'What We Did?'}</h1>
         <div className="mountains"></div>
         <div className="planets"></div>
