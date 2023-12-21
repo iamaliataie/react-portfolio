ff1f571e0465eaa375bff01b8601a775742603f5
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-21 11:38:51 +0430

    Update Pallax jsx

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -17,7 +17,7 @@ const Parallax = ({type}) => {
     <div ref={ref} className='parallax' style={{background: type==='services' ? 'linear-gradient(180deg, #111132, #0c0c1d)' : 'linear-gradient(180deg, #111132, #505064)'}}>
         <motion.h1 style={{ y:yText }}>{type === 'services' ? 'What We Do?' : 'What We Did?'}</motion.h1>
         <motion.div className="mountains"></motion.div>
-        <motion.div style={{ y:yBg}} className="planets"></motion.div>
+        <motion.div style={{ y:yBg, backgroundImage: `url(${type==='services' ? './planets.png' : '/sun.png'})`}} className="planets"></motion.div>
         <motion.div style={{ x:yBg}} className="stars"></motion.div>
     </div>
   )
