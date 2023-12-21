394d6621456e52717c1cce698fff3372b8f9fc28
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-21 11:00:44 +0430

    Update Pallax jsx

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,9 +1,16 @@
+import { useRef } from 'react';
 import './parallax.scss';
-import { motion } from 'framer-motion'
+import { motion, useScroll } from 'framer-motion'
 
 const Parallax = ({type}) => {
+
+  const ref = useRef()
+  const { scrollYProgress } = useScroll({
+    target: ref
+  })
+
   return (
-    <div className='parallax' style={{background: type==='services' ? 'linear-gradient(180deg, #111132, #0c0c1d)' : 'linear-gradient(180deg, #111132, #505064)'}}>
+    <div ref={ref} className='parallax' style={{background: type==='services' ? 'linear-gradient(180deg, #111132, #0c0c1d)' : 'linear-gradient(180deg, #111132, #505064)'}}>
         <motion.h1>{type === 'services' ? 'What We Do?' : 'What We Did?'}</motion.h1>
         <motion.div className="mountains"></motion.div>
         <motion.div className="planets"></motion.div>
