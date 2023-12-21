797ccf5d4114cf342ab8bfc4facd60f3b9f01654
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-21 11:35:24 +0430

    Update Pallax jsx

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -10,14 +10,15 @@ const Parallax = ({type}) => {
     offset: ['start start', 'end start']
   })
 
-  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "500%"])
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"])
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
 
   return (
     <div ref={ref} className='parallax' style={{background: type==='services' ? 'linear-gradient(180deg, #111132, #0c0c1d)' : 'linear-gradient(180deg, #111132, #505064)'}}>
-        <motion.h1 style={{ y:yBg }}>{type === 'services' ? 'What We Do?' : 'What We Did?'}</motion.h1>
+        <motion.h1 style={{ y:yText }}>{type === 'services' ? 'What We Do?' : 'What We Did?'}</motion.h1>
         <motion.div className="mountains"></motion.div>
-        <motion.div className="planets"></motion.div>
-        <motion.div className="stars"></motion.div>
+        <motion.div style={{ y:yBg}} className="planets"></motion.div>
+        <motion.div style={{ x:yBg}} className="stars"></motion.div>
     </div>
   )
 }
