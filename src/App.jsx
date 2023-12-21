2434f92774a0d2c8e08e7525432bdd304d29095f
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-21 10:40:41 +0430

    Add parallax to app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./app.scss"
 import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/navbar"
+import Parallax from "./components/parallax/Parallax"
 
 const App = () => {
   return (
@@ -10,7 +11,10 @@ const App = () => {
          <Hero/>
       </section>
       <section>Parallax</section>
-      <section id="Services">Services</section>
+      <section id="Services">
+        <Parallax type="services" />
+        <Parallax type="services" />
+      </section>
       <section>Parallax</section>
       <section id="Portfolio">Portfolio</section>
       <section>Portfolio1</section>
