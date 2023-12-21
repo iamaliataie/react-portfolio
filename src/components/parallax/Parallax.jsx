f68d258356a82758c9fdad029e828bef8d3b95a0
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-21 11:10:02 +0430

    Update Pallax jsx

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,17 +1,20 @@
 import { useRef } from 'react';
 import './parallax.scss';
-import { motion, useScroll } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
 
 const Parallax = ({type}) => {
 
   const ref = useRef()
   const { scrollYProgress } = useScroll({
-    target: ref
+    target: ref,
+    offset: ['start start', 'end start']
   })
+
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "500%"])
 
   return (
     <div ref={ref} className='parallax' style={{background: type==='services' ? 'linear-gradient(180deg, #111132, #0c0c1d)' : 'linear-gradient(180deg, #111132, #505064)'}}>
-        <motion.h1>{type === 'services' ? 'What We Do?' : 'What We Did?'}</motion.h1>
+        <motion.h1 style={{ y:yBg }}>{type === 'services' ? 'What We Do?' : 'What We Did?'}</motion.h1>
         <motion.div className="mountains"></motion.div>
         <motion.div className="planets"></motion.div>
         <motion.div className="stars"></motion.div>
