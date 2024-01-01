0eade2c9d4089897b5a8b6ebdd7c9c18038406da
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-01-01 11:05:23 +0430

    Update app jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,12 @@ const itmes = [
         img: 'https://images.unsplash.com/photo-1703934810573-a24c0db7d7e8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8',
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptate, veniam similique ipsa dicta aut aliquid esse animi porro laboriosam. Beatae cumque nisi, aliquid ducimus impedit sunt temporibus sequi itaque?'
     },
+    {
+        id: 2,
+        title: 'Next.js Blog',
+        img: 'https://images.unsplash.com/photo-1703994643629-6d0d56ee7a0f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptate, veniam similique ipsa dicta aut aliquid esse animi porro laboriosam. Beatae cumque nisi, aliquid ducimus impedit sunt temporibus sequi itaque?'
+    },
 ]
 
 const Portfolio = () => {
