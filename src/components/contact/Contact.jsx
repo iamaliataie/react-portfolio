de2a1a696d9da8bf05f446c9a481e9ae30b3f99e
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2024-01-06 20:13:40 +0430

    Update contact jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,6 +5,10 @@ const Contact = () => {
       <div className='contact'>
           <div className="text-conta">
               <h1>Let's work together</h1>
+              <div className="item">
+                  <h2>Mail</h2>
+                  <span>hello@react.com</span>
+              </div>
           </div>
     </div>
   )
